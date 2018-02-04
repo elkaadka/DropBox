a71513c55ebedb4cfbaf11abd192af8b479c3cca
adil <adil.kanabi@gmail.com>
2018-02-04 16:00:29 +0100

    Fix tests

--- spec/Kanel/DropBox/ClientSpec.php
+++ spec/Kanel/DropBox/ClientSpec.php
@@ -4,6 +4,7 @@ namespace spec\Kanel\DropBox;
 
 use Kanel\DropBox\Client;
 use Kanel\DropBox\Config;
+use Kanel\DropBox\Exceptions\DropBoxException;
 use Kanel\DropBox\Exceptions\FileNotFoundException;
 use PhpSpec\ObjectBehavior;
 use const \Kanel\DropBox\FILE_EXISTS;
@@ -16,15 +17,10 @@ class ClientSpec extends ObjectBehavior
         $this->beConstructedWith('my_access_token');
     }
 
-    function it_should_be_able_to_construct_with_config_object(Config $config)
-    {
-        $this->beConstructedWith($config);
-    }
-
     function it_should_throw_exception_if_file_does_not_exist()
     {
         $this->beConstructedWith('my_access_token');
 
-        $this->shouldThrow(FileNotFoundException::class)->during('upload', [FILE_DOES_NOT_EXISTS]);
+        $this->shouldThrow(DropBoxException::class)->during('upload', [FILE_DOES_NOT_EXISTS]);
     }
 }
